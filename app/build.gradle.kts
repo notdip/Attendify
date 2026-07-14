ce410686842372203322daf76b1488e239d04b56
notdip <pastedipesh24@gmail.com>
2026-07-14 09:27:09 +0530

    chore: bump versionCode to 3, versionName to "2.1.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId   = "com.dip.attendify"
         minSdk          = 26
         targetSdk       = 35
-        versionCode     = 2
-        versionName     = "2.1.0"
+        versionCode     = 3
+        versionName     = "2.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
